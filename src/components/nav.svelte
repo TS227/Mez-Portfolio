edc674b6fbc721300433cd7c63baa8505e370792
TS227 <tomstokes227@gmail.com>
2024-02-01 22:33:28 +0000

    Adds basic nav and icons

---
+++ src/components/nav.svelte
@@ -0,0 +1,17 @@
+<nav class='w-100 p-4'>
+	<div class='flex justify-between'>
+		<div class='w-100'>
+			<a href="/"><h1 class='text-3xl font-bold'>Mary Ann Hadley</h1></a>
+		</div>
+		<ul class='flex justify-center'>
+				<li class='px-3 text-xl'><a href="/">Home</a></li>
+				<li class='px-3 text-xl'><a href="/graphic design">Graphic Design</a></li>
+				<li class='px-3 text-xl'><a href="/blog">Art</a></li>
+				<li class='px-3 text-xl'><a href="/contact">Contact Me</a></li>
+		</ul>
+		<div class='w-100 flex justify-end'>
+			<a href="https://www.instagram.com/_mhadley" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram fa-2x"></i></a>
+			<a class='px-2' href="https://www.linkedin.com/in/mary-ann-hadley/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin fa-2x"></i></a>
+		</div>
+	</div>
+</nav>
